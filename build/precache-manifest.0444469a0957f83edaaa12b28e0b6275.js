036c64d169318602f4b98885e9145d3ddb52c321
Mohammad Sidani <sippi@Mohammads-MacBook-Pro.local>
2019-07-06 03:14:07 +0300

    add build

---
+++ build/precache-manifest.0444469a0957f83edaaa12b28e0b6275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340f3fb3cf1d7af3e07f342b23770b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a871403980ea734958cc",
+    "url": "/static/css/main.102d4ba5.chunk.css"
+  },
+  {
+    "revision": "b1d13997554082a598b2",
+    "url": "/static/js/2.2abfc311.chunk.js"
+  },
+  {
+    "revision": "a871403980ea734958cc",
+    "url": "/static/js/main.367f4fda.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
