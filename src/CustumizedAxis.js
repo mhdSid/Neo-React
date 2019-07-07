9e312de27662d75bd9761d14df32e04634264b18
mhdSid <mohdsidani@gmail.com>
2019-07-07 04:48:04 +0300

    updates

--- src/CustumizedAxis.js
+++ src/CustumizedAxis.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 function CustomizedLabel (props) {
-    const {
-    x, y, stroke, value,
-    } = props;
-
+    const { x, y, stroke, value } = props;
     return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">{value}</text>;
 }
 
 function renderLegend (props) {
-    // const { payload } = props;
-
     return (
         <ul className='chart-legend-ul flex flex-row align-center justify-center'>
             <li className='chart-legend-li flex flex-column'>
@@ -21,36 +16,25 @@ function renderLegend (props) {
                 <span>BENCHMARK</span>
                 <span className='chart-legend-icon'>$--</span>
             </li>
-        {/* {
-            payload.map((entry, index) => (
-            <li className='chart-legend-li' key={`item-${index}`}>{entry.value}</li>
-            ))
-        } */}
         </ul>
     );
 };
 
 function CustomizedAxisTickX (props) {
-    const {
-    x, y, payload, //stroke
-    } = props;
-
+    const { x, y, payload } = props;
     return (
-    <g transform={`translate(${x},${y})`}>
-        <text x={0} y={0} dy={16} fontSize='12' textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
-    </g>
+        <g transform={`translate(${x},${y})`}>
+            <text x={0} y={0} dy={16} fontSize='12' textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+        </g>
     );
 }
 
 function CustomizedAxisTickY (props) {
-    const {
-    x, y, payload, //stroke
-    } = props;
-
+    const { x, y, payload } = props;
     return (
-    <g transform={`translate(${x},${y - 10})`}>
-        <text x={0} y={0} dy={16} fontSize='12' textAnchor="end" fill="#666" >{payload.value}</text>
-    </g>
+        <g transform={`translate(${x},${y - 10})`}>
+            <text x={0} y={0} dy={16} fontSize='12' textAnchor="end" fill="#666" >{payload.value}</text>
+        </g>
     );
 }
 
